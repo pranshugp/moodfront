49e5161cb4d79ea5398219806d29c8ea66e656a0
Pranshu Gupta <111183216+pranshugp@users.noreply.github.com>
2024-11-09 00:02:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
   const fetchBooks = async (selectedMood) => {
     try {
-      const response = await axios.get(`http://localhost:3000/recommendation/${selectedMood}`);
+      const response = await axios.get(`https://moodapp-aplk.onrender.com`);
       setBooks(response.data);
       setError("");
     } catch (error) {
